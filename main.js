b8c46871506833c85bb50a61ecc4d59981992be5
Duncannjau <njaugitahi@gmail.com>
2022-09-14 19:42:53 +0300

    Add display function for the popup menu in js

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ const btn = document.querySelector('#btn-menu');
 function displayMobileMenu() {
   document.querySelector('#overlay').style.display = 'block';
 }
+const btn = document.querySelector('#btn-menu');
+
+function displayMobileMenu() {
+  document.querySelector('#overlay').style.display = 'block';
+}
 
